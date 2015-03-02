86bbcb34192a14ac4160e8271bb90ad6e09d8557
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-02 10:07:30 -0500

    Header

--- templatePage.php
+++ templatePage.php
@@ -7,6 +7,36 @@
 		<title>Template</title>
 	</head>
 	</body>
-		
+		<header>
+			<div id="titre_logo">
+					<a href="http://cll.qc.ca" target="_blank"><img src="bck_logo.png" alt="Cégep Lévis-Lauzon" /></a>
+			</div>
+			
+			<div id="titre_principal">
+			<h1>Techniques de l'informatique </h1>
+			</div>
+			
+			<div id="titre_menu">
+			
+				<ul id="menu">
+					<li><a href="#1">Accès Rapides</a>
+						<ul class="menuderoulant">
+							<li><a href="#2">Omnivox </a></li>
+							<li><a href="#3">Messagerie</a></li>
+							<li><a href="#4">Repro+</a></li>
+							<li><a href="#5">Calendrier scolaire</a></li>
+							<li><a href="#6">Repertoire du personnel</a></li>
+							<li><a href="#7">Stage en entreprise</a></li>
+							<li><a href="http://cll.qc.ca/programmes/alternance-travail-etudes/"  target="_blank" >Alternance Travail-Études</a></li>
+							<li><a href="http://www.clevislauzon.qc.ca/informatique/PDEA%20Version%20officielle%202010.pdf" target="_blank" >PDÉA</a></li>
+							<li><a href="#8">Accueil</a></li>
+							<li><a href="#9">Site du cégep</a></li>
+							<li><a href="#9">Facebook</a></li>
+							<li><a href="#10">Nous joindre</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</header>
 	</body>
 </html>
