1c20d09567cfaa3f3902f683acf2e64ef1cdc694
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-02 09:56:35 -0500

    Imgs for Header

--- templatePage.php
+++ templatePage.php
@@ -1,11 +1,12 @@
 <html>
-<head>
-<?php
-header('Content-Type: text/html; charset=utf-8');
-setlocale(LC_ALL, 'fr_CA');
-?>
-<title>Template</title>
-</head>
-</body>
-</body>
+	<head>
+		<?php
+		header('Content-Type: text/html; charset=utf-8');
+		setlocale(LC_ALL, 'fr_CA');
+		?>	
+		<title>Template</title>
+	</head>
+	</body>
+		
+	</body>
 </html>
