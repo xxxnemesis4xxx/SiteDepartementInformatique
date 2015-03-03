5e706919a4b3e3f0ec03b604333608a86ffd633e
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-03 09:15:26 -0500

    menu dynamic link

--- templatePage.php
+++ templatePage.php
@@ -1,8 +1,8 @@
 <html>
 	<head>
 		<?php
-		header('Content-Type: text/html; charset=utf-8');
-		setlocale(LC_ALL, 'fr_CA');
+			header('Content-Type: text/html; charset=utf-8');
+			setlocale(LC_ALL, 'fr_CA');
 		?>	
 		<title>Template</title>
 		<link rel="stylesheet" href="css/templatePage.css">
@@ -22,39 +22,34 @@
 						<span>Accès Rapides</span>
 					</a>
 						<ul class="menuderoulant">
-							<li><a href="https://cll.omnivox.ca/intr/Module/Identification/Login/Login.aspx?ReturnUrl=/intr">
-								<span>Omnivox</span> 
-							</a></li>
-							<li><a href="https://alphonse2.clevislauzon.qc.ca/gw/webacc">
-								<span>Messagerie</span>
-							</a></li>
-							<li><a href="https://repro.clevislauzon.qc.ca/sysrepro/login/login.asp">
-								<span>Repro+</span>
-							</a></li>
-							<li><a href="http://cll.qc.ca/repertoire-personnel/">
-								<span>Repertoire du personnel</span>
-							</a></li>
-							<li><a href="#7">
-								<span>Stage en entreprise</span>
-							</a></li>
-							<li><a href="http://cll.qc.ca/programmes/alternance-travail-etudes/"  target="_blank" >
-								<span>Alternance Travail-Études</span>
-							</a></li>
-							<li><a href="http://www.clevislauzon.qc.ca/informatique/PDEA%20Version%20officielle%202010.pdf" target="_blank" >
-								<span>PDÉA</span>
-							</a></li>
-							<li><a href="http://www.cll.qc.ca/informatique/index2.php">
-								<span>Accueil</span>
-							</a></li>
-							<li><a href="http://cll.qc.ca/">
-								<span>Site du cégep</span>
-							</a></li>
-							<li><a href="https://www.facebook.com/pages/D%C3%A9partement-dinformatique-C%C3%A9gep-L%C3%A9vis-Lauzon/127878897268010">
-								<span>Facebook</span>
-							</a></li>
-							<li><a href="#bas">
-								<span>Nous joindre</span>
-							</a></li>
+							<?php
+							$servername = "localhost";
+							$username = "root";
+							$password = "admin123*";
+							$dbname = "departementinformatique";
+				
+							// Create connection
+							$conn = new mysqli($servername, $username, $password, $dbname);
+							// Check connection
+							if ($conn->connect_error) {
+									die("Erreur de connection: " . $conn->connect_error);
+							} 
+							
+							$sql = "SELECT * from lienmenuderoulant;";
+							$result = $conn->query($sql);
+				
+							if ($result->num_rows > 0) {
+								// output data of each row
+								while($row = $result->fetch_assoc()) {
+								    echo "<li><a href='". $row["lien"] . "' ><span>" . utf8_encode($row["nomLien"]) . "</span></a></li>";
+								}
+							} else {
+									echo "0 results";
+							}
+				
+				
+							$conn->close();
+						?>
 						</ul>
 					</li>
 				</ul>
