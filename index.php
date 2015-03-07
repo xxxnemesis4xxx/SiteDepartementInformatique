6d41f790f67bbf9fa1c81a7e9b493a5c697c6153
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-07 16:12:03 -0500

    Default Index Page

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		 <?php include("defaultinclude.txt"); ?>
+		 <title>Accueil</title>
+	</head>
+	<body>
+		<?php include("header.txt"); ?>
+		<?php include("footer.txt"); ?>
+	</body>
+</html>
