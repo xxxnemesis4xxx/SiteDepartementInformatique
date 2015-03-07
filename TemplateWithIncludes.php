37fbca67709e046dc3d092001ff2e4cc63023054
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-07 16:00:06 -0500

    Basic Example + Final Commit

---
+++ TemplateWithIncludes.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+ <?php include("defaultinclude.txt"); ?>
+</head>
+<body>
+<?php include("header.txt"); ?>
+<?php include("verticalmenu.txt"); ?>
+<?php include("footer.txt"); ?>
+</body>
+</html>
