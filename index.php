1d46dd0d81c3badc775d0ae511225f152b5122f1
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-08 11:18:37 -0400

    100% dynamic Index - Final commit

--- index.php
+++ index.php
@@ -18,6 +18,48 @@
 		<?php include("header.txt"); ?>
 		
 		<table class="indexTable">
+		
+		<?php
+			$servername = "localhost";
+			$username = "root";
+			$password = "admin123*";
+			$dbname = "departementinformatique";
+			
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			if ($conn->connect_error) {
+					die("Erreur de connection: " . $conn->connect_error);
+			} 
+			
+			$sql = "select * from displaymenu order by position;";
+			$result = $conn->query($sql);
+			
+			if ($result->num_rows > 0) {
+				$firstValue = true;
+				$current = $result->fetch_assoc();
+				while ($current != null) {
+					$nextValue = $result->fetch_assoc();
+					if ($firstValue == true) {
+						echo "<tr>";
+						$firstValue = false;
+					}
+					echo "<td colspan='" . $current["colspan"] . "' rowspan='" . $current["rowspan"]  . "'>";
+					include ($current["nomFichier"]);
+					echo "</td>";
+					if ($current["lastCols"] == true and $nextValue != null) {
+						echo "</tr><tr>";
+					}
+					if ($current != null and $nextValue == null) {
+						echo "</tr>";
+					}
+					
+					$current = $nextValue;
+				}
+			}
+			
+			$conn->close();
+		?>
 		<!--
 			<tr>
 				<td>
