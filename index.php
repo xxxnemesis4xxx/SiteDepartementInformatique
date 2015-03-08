d71c2a8d32676a4edb4a3b01b8aee8e4e2b3e18f
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-07 19:52:59 -0500

    Default Table For CSS Purpose

--- index.php
+++ index.php
@@ -5,6 +5,30 @@
 	</head>
 	<body>
 		<?php include("header.txt"); ?>
+		
+		<table>
+			<tr>
+				<td>Element 1</td>
+				<td>Element 2</td>
+				<td>Element 3</td>
+				<td>Element 4</td>
+				<td>Element 5</td>
+			</tr>
+			<tr>
+				<td rowspan="2">Element 1</td>
+				<td>Element 2</td>
+				<td>Element 3</td>
+				<td colspan="2" style="text-align:center">Element 4</td>
+			</tr>
+			<tr>
+				<td>Element 2</td>
+				<td>Element 3</td>
+				<td>Element 4</td>
+				<td>Element 5</td>
+			</tr>
+		</table>
+		
+		
 		<?php include("footer.txt"); ?>
 	</body>
 </html>
