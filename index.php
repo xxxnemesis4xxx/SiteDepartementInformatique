940a37d53a05be6f679e779089613b7f68303e1e
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-08 17:41:28 -0400

    Fixed Slider!

--- index.php
+++ index.php
@@ -3,15 +3,7 @@
 		 <?php include("defaultinclude.txt"); ?>
 		 <title>Accueil</title>
 		 <link rel="stylesheet" href="css/index.css">
-		 <script src="javascript/jquery-1.11.2.min.js"></script>
-		 <script src="javascript/unslider.min.js"></script>
 		
-		 
-		 <script>
-		 	$(function() {
-			    $('.slider').unslider();
-			});
-		 </script>
 	</head>
 	
 	<body>
