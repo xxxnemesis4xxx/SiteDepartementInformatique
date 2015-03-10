05e90ba1188f03a2e3eea191e2bf9262e2e1878e
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-10 13:34:59 -0400

    Created Default Stage Page

---
+++ stage.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+	 	<?php
+	 		include("defaultinclude.txt"); 
+	 	?>
+	 	<title>
+	 		Stage
+	 	</title>
+	</head>
+	<body>
+		<?php
+			include("header.txt");
+		?>
+		<?php 
+			include("verticalmenu.txt");
+		?>
+		<?php 
+			include("footer.txt");
+		?>
+	</body>
+</html>
