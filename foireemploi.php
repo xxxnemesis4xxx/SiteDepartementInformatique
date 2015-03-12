51909f3030efad4e0eeb05f49248681951ccf37e
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-12 17:11:30 -0400

    Foire Done

---
+++ foireemploi.php
@@ -0,0 +1,55 @@
+<html>
+	<head>
+	 	<?php
+	 		include("defaultinclude.txt"); 
+	 	?>
+	 	<title>
+	 		Foire de l'emploi
+	 	</title>
+	</head>
+	<body>
+		<?php
+			include("header.txt");
+		?>
+		<?php 
+			include("verticalmenu.txt");
+		?>
+			<div class="contenu">
+				<h1 class="titreTemplate">Foire de l'emploi</h1>
+				<p class="textTemplate">
+					Cet évènement vous permettra de rencontrer les finissantes et finissants des programmes suivants : 
+					Informatique de gestion, Informatique industrielle et Gestion de réseaux informatiques. Tous ces étudiants ont à compléter,
+					dans la prochaine année, un stage de fin d’études et souhaitent obtenir de l’information sur votre établissement et sur
+					les opportunités de stage et d’emploi qui leur sont offertes.
+					<br/><br/>
+					À titre d’information, voici les dates prévues de début de stage :<br/>
+					- Informatique de gestion : 30 mars 2015 (pour 8, 15 ou 21 semaines)<br/>
+					- Informatique industrielle : 7 avril 2015 (pour 7, 15 ou 20 semaines)<br/>
+					- Gestion de réseaux informatiques : 26 janvier 2015 (pour 16 semaines)<br/><br/>
+					
+					De plus, nous inviterons aussi les étudiants de 1ère et de 2ème année à se présenter au Salon afin de vous rencontrer.
+					Ceux-ci ont la possibilité de réaliser durant leur formation un stage d’été (ATE). Ce sera aussi pour eux une occasion
+					de connaître le type d’emplois qu’ils pourraient occuper en informatique.
+					<br/><br/>
+					À titre d’indication, voici le nombre d’étudiants par concentration et par année :<br/>
+					- Finissants en informatique de gestion : 13<br/>
+					- Finissants en informatique industrielle : 6<br/>
+					- Finissants en gestion de réseaux : 8<br/>
+					- 2e année (tronc commun gestion/industrielle) : 21<br/>
+					- 2e année - gestion de réseaux : 5<br/>
+					- 1ère année : 61<br/><br/>
+					
+					Toutes les institutions participantes bénéficieront d’un espace pour un kiosque de 10’ X 10’ dans le mail central du Cégep, où se déroulera l’activité.<br/><br/>
+					
+					Afin de faciliter la planification de cette activité, nous vous saurions gré de bien vouloir confirmer votre présence par courriel au plus tard le jeudi 25 septembre 2014.
+					<br/><br/>
+					<a class="facebook" target="_blank" href="https://www.facebook.com/events/712070928885637/" onclick="javascript:_gaq.push(['_trackEvent','outbound-menu','http://www.facebook.com']);">
+					</a>
+				</p>
+		</div>
+		<div class="contentFix"></div>
+		<?php 
+			include("footer.txt");
+		?>
+	</body>
+</html>
