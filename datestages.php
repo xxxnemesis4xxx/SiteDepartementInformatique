bd0115335ce8049dcfcaebc17ddc3d995b0e906b
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-12 16:06:59 -0400

    Dates des Stages Done

--- datestages.php
+++ datestages.php
@@ -16,23 +16,22 @@
 		?>
 			<div class="contenu">
 				<h1 class="titreTemplate">Dates des stages</h1>
+				<h2 class="sousTitre">Informatique de gestion</h2>
 				<p class="textTemplate">
-					Pour les stages en informatique de gestion<br/><br/>
-	
-					Stage de 15 semaines ou 21 semaines à rémunération obligatoire : 30 mars au 10 juillet 2015 ou jusqu'au 21 août 2015
-					Stages de 8 semaines à rémunération obligatoire  : 30 mars au 22 mai 2015<br/><br/><br/>
+					Stages de 8 semaines à rémunération obligatoire  : 30 mars au 22 mai 2015<br/>
+					Stage de 15 semaines ou 21 semaines à rémunération obligatoire : 30 mars au 10 juillet 2015 ou jusqu'au 21 août 2015<br/>
+				</p>
 					
 					
-					Pour les stages en informatique industrielle<br/><br/>
+				<h2 class="sousTitre">Pour les stages en informatique industrielle</h2>
+				<p class="textTemplate">
+					Stage de 7 semaines à rémunération obligatoire : 6 avril au 22 mai 2015<br/>
+					Stage de 15 semaines ou 20 semaines à rémunération obligatoire : du 6 avril au 17 juillet 2015 ou jusqu’au 21 août 2015<br/>
+				</p>
 					
-					Stage de 7 semaines à rémunération obligatoire : 6 avril au 22 mai 2015
-					Stage de 15 semaines ou 20 semaines à rémunération obligatoire : du 6 avril au 17 juillet 2015 ou jusqu’au 21 août 2015<br/><br/><br/>
-					
-					
-					Pour les stages en gestion de réseaux<br/><br/>
-					
+				<h2 class="sousTitre">Pour les stages en gestion de réseaux</h2>
+				<p class="textTemplate">
 					Stage de 16 semaines à rémunération obligatoire : date à déterminer
-					<br/>
 				</p>
 		</div>
 		<div class="contentFix"></div>
