95dbfe38d7a98ccac4d5b20c9073e3b9e2de7811
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-12 17:00:46 -0400

    Activites Done

---
+++ activite.php
@@ -0,0 +1,62 @@
+<html>
+	<head>
+	 	<?php
+	 		include("defaultinclude.txt"); 
+	 	?>
+	 	<title>
+	 		Activités 
+	 	</title>
+	</head>
+	<body>
+		<?php
+			include("header.txt");
+		?>
+		<?php 
+			include("verticalmenu.txt");
+		?>
+			<div class="contenu">
+				<h1 class="titreTemplate">Activités </h1>
+				<h2 class="sousTitre">Le Ballon-Balai</h2>
+				<p class="textTemplate">
+					Durant la session d'hiver, le département d'informatique organise du ballon-balai amical sur la patinoire extérieure derrière le Cégep. Venez
+					vous joindre à nous ! C'est drôle, facile, et bon pour le cardio !
+					<br/><br/>
+					Non-compétitif		<br/>		
+					- Sans danger pour votre vie !<br/> 
+					- Équipement fourni<br/>
+					- Tout le monde joue et la moitié gagne !<br/>
+					- Ballon de mousse<br/> 
+					- Venir en bottes !<br/>
+				</p>
+						
+				<h2 class="sousTitre">Le Hockey Cossom</h2>
+				<p class="textTemplate">
+					À chaque année, le département d'informatique réserve le gymnase un soir par semaine, durant la session d'automne
+					et au printemps, pour jouer au hockey cossom. C'est une façon de faire sortir le "méchant" tout en travaillant pour
+					l'amélioration de sa forme physique !
+				</p>
+					
+				<h2 class="sousTitre">Le Hockey sur glace</h2>
+				<p class="textTemplate">
+					Quelques fois durant l'hiver, l'aréna de Lauzon est loué par le département pour permettre une partie de hockey
+					amicale avec les étudiants et les professeurs. Une activité très appréciée de la plupart des étudiants.
+				</p>
+				<h2 class="sousTitre">Le Rallye</h2>
+				<p class="textTemplate">
+					Aahhh le rallye! Y aller c'est l'adopter ! Alors, si vous êtes nouveau, ne vous contentez pas de passer à travers
+					la technique sans faire aucune activité, venez nous découvrir au rallye qui a lieu à chaque début d'année (septembre).
+				</p>
+				
+				<h2 class="sousTitre">Les Partys !</h2>
+				<p class="textTemplate">
+					Arrosés de houblon ou de fort, les partys en informatique sont toujours les bienvenus quand vient le temps d'oublier un
+					examen plus ou moins réussi ou un virus bien implanté dans votre ordinateur qui vous a obligé à tout formatter... Bref,
+					toutes les raisons sont bonnes pour ne pas manquer un de ces partys !
+				</p>
+		</div>
+		<div class="contentFix"></div>
+		<?php 
+			include("footer.txt");
+		?>
+	</body>
+</html>
