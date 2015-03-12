b99c6d531a4af1eefb3e64c4af6fc1c922414893
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-12 16:35:06 -0400

    Formation Done

---
+++ formationetudiant.php
@@ -0,0 +1,58 @@
+<html>
+	<head>
+	 	<?php
+	 		include("defaultinclude.txt"); 
+	 	?>
+	 	<title>
+	 		Critères 
+	 	</title>          
+	</head>                       
+	<body>                                              
+		<?php
+			include("header.txt");
+		?>                      
+		<?php 
+			include("verticalmenu.txt");
+		?>
+		<div class="contenu">
+			<h1 class="titreTemplate">Formation des étudiants</h1>
+			<p class="textTemplate">
+				Les 2 premières sessions sont communes aux 3 programmes.<br/>
+				Les étudiant(es) ont acquis les notions de base propres à l'informatique:
+				<ul>                               
+					<li>» programmation(C#, Html, Javascript, Word, Excel)</li>       
+					<li>» logiciels de base</li>  
+					<li>» fonctionnement interne des ordinateurs</li>
+					<li>» Internet</li>
+				</ul>
+			</p>                                                                                                            
+			<p class="textTemplate">                         
+					<br/>Pour l'informatique de gestion et l'informatique industrielle, la 3e et la 4e session est enrichie de:
+								
+					<ul>
+						<li>» Programmation par objet   </li>    
+						<li>» Système d'exploitation, Windows XP</li>   
+						<li>» Oracle</li> 
+						<li>» Analyse UML</li>                            
+						<li>» Analyse Réseaux (soutien technique)</li> 
+					</ul>                                     
+			</p>
+			<p class="textTemplate">
+				<br/>
+				Et finalement ils ont 2 sessions de formation spécifique dans l'une ou l'autre voie:
+				<ul>                                    
+					<li>» Informatique de Gestion   </li>
+					<li>» Informatique Industrielle </li>
+				</ul>
+				                              
+			</p>
+			<p class="textTemplate">
+				<br/>Pour Gestion de réseaux, les 4 dernières sessions sont spécifiques à leur programme
+			</p>
+		</div>
+		<div class="contentFix"></div>
+		<?php 
+			include("footer.txt");
+		?>
+	</body>
+</html>
