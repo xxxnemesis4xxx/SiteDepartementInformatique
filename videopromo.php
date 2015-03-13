8811619d316d95b5561df64f69fd68fe23e7b31b
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-13 00:23:34 -0400

    Video Done

---
+++ videopromo.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+	 	<?php
+	 		include("defaultinclude.txt"); 
+	 	?>
+	 	<title>
+	 		Vidéo promotionnelle 
+	 	</title>
+	</head>
+	<body>
+		<?php
+			include("header.txt");
+		?>
+		<?php 
+			include("verticalmenu.txt");
+		?>
+			<div class="contenu">
+				<h1 class="titreTemplate">Vidéo(s) promotionnelle(s)</h1>
+				<p class="textTemplate">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/Xr1X-WryNPM" frameborder="0" allowfullscreen></iframe>
+				</p>
+		</div>
+		<div class="contentFix"></div>
+		<?php 
+			include("footer.txt");
+		?>
+	</body>
+</html>
