00fed7e195500ad4d02f36b30673de3ff1352f4c
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-15 06:59:43 -0400

    Update php teachers

--- enseignant/guillaumemichaud.php
+++ enseignant/guillaumemichaud.php
@@ -6,15 +6,43 @@
 	 	<title>
 	 		Enseignant 
 	 	</title>
+		
+		<script src="/projet/h2015/equipe6/ckeditor/ckeditor.js"></script>
+		
+		<script>
+			$(function() {
+				CKEDITOR.disableAutoInline = true;
+				CKEDITOR.inline( 'editor1' );
+			});
+			
+			$(document).ready(function(){
+				$('.button').click(function(){
+					var content = CKEDITOR.instances.editor1.getData();
+					var ajaxurl = 'ajax.php',
+					data =  {'action': content,'fichier' : 'guillaumemichaud.txt'};
+					$.post(ajaxurl, data, function (response) {
+						alert("Sauvegarde Réussi!");
+					});
+				});
+			});
+		</script>
 	</head>
 	<body>
 		<?php
 			include("../header.php");
 			include("../verticalmenu.php");
 		?>
+		
 			<div class="contenu">
+			<form>
+					<div name="editor1" id="editor1" contenteditable="true">
+						<?php include("guillaumemichaud.txt"); ?>
+					</div>
+					
+					<input type="submit" class="button" name="insert" value="Sauvegarde" />
+			</form>
 			</div>
-		</div>
+		
 		<div class="contentFix"></div>
 		<?php 
 			include("../footer.php");
