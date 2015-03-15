43f30a377a14c3be692ae094189487bedb9be6d3
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-14 21:18:32 -0400

    Update includes teacher Pages

--- enseignant/olivierlafleur.php
+++ enseignant/olivierlafleur.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 	 	<?php
-	 		include("defaultinclude.php"); 
+	 		include("../defaultinclude.php"); 
 	 	?>
 	 	<title>
 	 		Enseignant 
@@ -9,17 +9,15 @@
 	</head>
 	<body>
 		<?php
-			include("header.php");
-		?>
-		<?php 
-			include("verticalmenu.php");
+			include("../header.php");
+			include("../verticalmenu.php");
 		?>
 			<div class="contenu">
 			</div>
 		</div>
 		<div class="contentFix"></div>
 		<?php 
-			include("footer.php");
+			include("../footer.php");
 		?>
 	</body>
 </html>
