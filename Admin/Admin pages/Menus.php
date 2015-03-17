4d70e94e8c862ca559243e7d809cd2a97b86de8e
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-16 21:22:49 -0400

    Default Page

---
+++ Admin/Admin pages/Menus.php
@@ -0,0 +1,12 @@
+<html>
+	<?php
+				header('Content-Type: text/html; charset=utf-8');
+				setlocale(LC_ALL, 'fr_CA');
+	?>
+	<head>
+		<title>Menu</title>
+	</head>
+	<body>
+		
+	</body>
+</html>
