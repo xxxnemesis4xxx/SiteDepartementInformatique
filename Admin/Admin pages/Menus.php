cebcb15db5386626600ed24996bac6e9bbe07228
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-17 16:57:46 -0400

    Display Vertical Menu

--- Admin/Admin pages/Menus.php
+++ Admin/Admin pages/Menus.php
@@ -268,8 +268,53 @@
 					<option value="sm23">Supprimer</option>
 				</select>
 				
+				<style>
+							ul, ul ul, ul ul ul {
+								list-style : none;
+							}
+						</style>
+						<ul><i>
+							<?php
+								$servername = "localhost";
+								$username = "equipe6h15";
+								$password = "ebola-info";
+								$dbname = "equipe6h15";
+							
+								// Create connection
+								$conn = new mysqli($servername, $username, $password, $dbname);
+								// Check connection
+								if ($conn->connect_error) {
+										die("Erreur de connection: " . $conn->connect_error);
+								} 
+								
+								$sql = "select * from verticalmenu order by renderHtmlPosition, layer,position;";
+								$result = $conn->query($sql);
+							
+								if ($result->num_rows > 0) {
+									$current = $result->fetch_assoc();
+									while($current != null) {
+										$next = $result->fetch_assoc();
+										if ($current["layer"] ==  $next["layer"]) {
+											echo "<li>Position : " . $current["position"] . " - " .utf8_encode($current["nomLien"]) ."</li>";
+										} elseif ($current["layer"] < $next["layer"]) {
+											echo "<li>Position : " . $current["position"] . " - " .utf8_encode($current["nomLien"]) . "<ul>";
+										} elseif ($current["layer"] > $next["layer"]) {
+											echo "<li>Position : " . $current["position"] . " - " . utf8_encode($current["nomLien"]) 
+											. (($current["layer"] - $next["layer"] == 1)?"</li></ul></li>":"</li></ul></li></ul></li>");
+										}
+												
+										$current = $next;
+									}
+								}
+								
+								$conn->close();
+							?>
+						</ul></i>
+				
 				<div id="sm21" style="display:block">
-					Hello Div 1
+					<form>
+						
+					<form>
 				</div>
 				
 				<div id="sm22" style="display:none">
