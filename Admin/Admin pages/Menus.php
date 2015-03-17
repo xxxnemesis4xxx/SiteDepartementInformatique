74f592ee60258a06bef86a1efa6b877c3930b0d4
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-17 02:48:55 -0400

    Saving Item in DB

--- Admin/Admin pages/Menus.php
+++ Admin/Admin pages/Menus.php
@@ -6,7 +6,30 @@
 	<head>
 		<title>Menu</title>
 		<script src="http://205.236.12.52/projet/h2015/equipe6/javascript/jquery-1.11.2.min.js"></script>
+		<script src="http://205.236.12.52/projet/h2015/equipe6/javascript/jquery-ui.min.js"></script>
+		<link rel="stylesheet" href="http://205.236.12.52/projet/h2015/equipe6/javascript/jquery-ui.min.css">
+		<link rel="stylesheet" href="http://205.236.12.52/projet/h2015/equipe6/javascript/jquery-ui.theme.min.css">
+
 		<script>
+			$(function() {
+				var spinner = $( "#position" ).spinner();
+				$( "#position" ).spinner( "option", "min", 1);
+			});
+			
+			$(document).ready(function(){
+				$('#ajouterlien').click(function(){
+					var method = $(this).val();
+					var nomlien = document.getElementById('nomlien').value;
+					var lien = document.getElementById('lien').value;
+					var position = document.getElementById('position').value;
+					var ajaxurl = 'adminfunctions.php',
+					data =  {'action': method, 'nomlien' : nomlien, 'lien' : lien, 'position' : position};
+					$.post(ajaxurl, data, function (response) {
+						location.reload(true);
+					});
+				});
+			});
+		
 			function showDivFirstMenu() {
 				var selectBox = document.getElementById('menu');
 				var divId = selectBox.options[selectBox.selectedIndex].value;
@@ -60,7 +83,46 @@
 				</select>
 				
 				<div id="sm11" style="display:block">
-					Hello Div 1
+					<form>
+						<br/><br/>
+						Titre du lien : <br/>
+						<input type="text" id="nomlien"/><br/>
+						Adresse du lien: <br/>
+						<input type="text" id="lien"/><br/>
+						Indiquer la position à laquelle vous souhaitez voir apparaître votre titre<br/>
+						<input id="position"/><br/>
+						<i>Voici la liste actuelle des liens avec leur position :
+						<ul style="list-style : none">
+							<?php
+								$servername = "localhost";
+								$username = "equipe6h15";
+								$password = "ebola-info";
+								$dbname = "equipe6h15";
+								
+								// Create connection
+								$conn = new mysqli($servername, $username, $password, $dbname);
+								// Check connection
+								if ($conn->connect_error) {
+										die("Erreur de connection: " . $conn->connect_error);
+								} 
+								
+								$sql = "select * from lienmenuderoulant order by renderHtmlPosition;";
+								$result = $conn->query($sql);
+								
+								if ($result->num_rows > 0) {
+									$Position = 1;
+									while ($current = $result->fetch_assoc()) {
+										echo "<li>" . $Position . " - " . utf8_encode($current["nomLien"]) . "</li>";
+										$Position++;
+									}
+								}
+								
+								$conn->close();
+							?>
+						</ul></i>
+						
+						<input type="button" id="ajouterlien" value="Ajouter Lien"/>
+					<form>
 				</div>
 				
 				<div id="sm12" style="display:none">
