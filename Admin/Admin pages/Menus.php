2f0b4a5517a90297337d0ace6fc96aa4bcc18340
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-16 22:22:23 -0400

    Show Options

--- Admin/Admin pages/Menus.php
+++ Admin/Admin pages/Menus.php
@@ -7,7 +7,7 @@
 		<title>Menu</title>
 		<script src="http://205.236.12.52/projet/h2015/equipe6/javascript/jquery-1.11.2.min.js"></script>
 		<script>
-			function showDiv() {
+			function showDivFirstMenu() {
 				var selectBox = document.getElementById('menu');
 				var divId = selectBox.options[selectBox.selectedIndex].value;
 				for(i = 0; i < selectBox.length; i++)
@@ -17,22 +17,82 @@
 				
 				document.getElementById(divId).style.display = "block";
 			}
+			
+			function showDivSecondMenu1() {
+				var selectBox = document.getElementById('deuxiememenu1');
+				var divId = selectBox.options[selectBox.selectedIndex].value;
+				for(i = 0; i < selectBox.length; i++)
+				{
+					document.getElementById(selectBox.options[i].value).style.display = "none";
+				}
+				
+				document.getElementById(divId).style.display = "block";
+			}
+			
+			function showDivSecondMenu2() {
+				var selectBox = document.getElementById('deuxiememenu2');
+				var divId = selectBox.options[selectBox.selectedIndex].value;
+				for(i = 0; i < selectBox.length; i++)
+				{
+					document.getElementById(selectBox.options[i].value).style.display = "none";
+				}
+				
+				document.getElementById(divId).style.display = "block";
+			}
+			
 		</script>
 	</head>
 	<body>
 		<p>
 			Qu'elle menu voulez-vous modifier ? <br/>
-			<select id="menu"onchange='showDiv()'>
+			<select id="menu" onchange='showDivFirstMenu()'>
 				<option value="menuhorizontale">Menu Horizontale</option>
 				<option value="menuverticale">Menu Vertical</option>
 			</select>
 			
 			<div id="menuhorizontale" style="display:block">
-				Bonjour World!
+				<br/><br/>
+				Choisir l'option que vous souhaitez ?<br/>
+				<select id="deuxiememenu1" onchange='showDivSecondMenu1()'>
+					<option value="sm11">Ajouter</option>
+					<option value="sm12">Modifier</option>
+					<option value="sm13">Supprimer</option>
+				</select>
+				
+				<div id="sm11" style="display:block">
+					Hello Div 1
+				</div>
+				
+				<div id="sm12" style="display:none">
+					Hello Div 2
+				</div>
+				
+				<div id="sm13" style="display:none">
+					Hello Div 3
+				</div>
+				
 			</div>
 			
 			<div id="menuverticale" style="display:none">
-				Hello Monde!
+				<br/><br/>
+				Choisir l'option que vous souhaitez ?<br/>
+				<select id="deuxiememenu2" onchange='showDivSecondMenu2()'>
+					<option value="sm21">Ajouter</option>
+					<option value="sm22">Modifier</option>
+					<option value="sm23">Supprimer</option>
+				</select>
+				
+				<div id="sm21" style="display:block">
+					Hello Div 1
+				</div>
+				
+				<div id="sm22" style="display:none">
+					Hello Div 2
+				</div>
+				
+				<div id="sm23" style="display:none">
+					Hello Div 3
+				</div>
 			</div>
 		</p>
 	</body>
