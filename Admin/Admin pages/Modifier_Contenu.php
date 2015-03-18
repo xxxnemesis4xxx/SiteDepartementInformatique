fe09801c466eceb84cb2518fba3ed49940fbe418
xxxnemesis4xxx <magnitude9@live.ca>
2015-03-17 23:49:32 -0400

    New Tab

---
+++ Admin/Admin pages/Modifier_Contenu.php
@@ -0,0 +1,12 @@
+<html>
+	<?php
+				header('Content-Type: text/html; charset=utf-8');
+				setlocale(LC_ALL, 'fr_CA');
+	?>
+	<head>
+		<title>Modifier Contenu</title>
+		<script src="http://205.236.12.52/projet/h2015/equipe6/javascript/jquery-1.11.2.min.js"></script>
+	</head>
+	<body>
+	</body>
+</html>
